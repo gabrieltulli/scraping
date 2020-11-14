34ee054601603f3593d70c280465d1d32ca5b0f7
gabrieltulli <gabrieltulli@users.noreply.github.com>
2020-11-13 22:25:59 -0300

    refactor

--- src/main/java/tulli/com/br/scrapingtool/Application.java
+++ src/main/java/tulli/com/br/scrapingtool/Application.java
@@ -3,53 +3,10 @@ package tulli.com.br.scrapingtool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableCaching
-@EnableAsync
-// We use direct @Import instead of @ComponentScan to speed up cold starts
-// @ComponentScan(basePackages = "tulli.com.br.controller")
 public class Application {
-
-//	/*
-//	 * Create required HandlerMapping, to avoid several default HandlerMapping
-//	 * instances being created
-//	 */
-//	@Bean
-//	public HandlerMapping handlerMapping() {
-//		return new RequestMappingHandlerMapping();
-//	}
-//
-//	/*
-//	 * Create required HandlerAdapter, to avoid several default HandlerAdapter
-//	 * instances being created
-//	 */
-//	@Bean
-//	public HandlerAdapter handlerAdapter() {
-//		return new RequestMappingHandlerAdapter();
-//	}
-//
-//	/*
-//	 * optimization - avoids creating default exception resolvers; not required as
-//	 * the serverless container handles all exceptions
-//	 *
-//	 * By default, an ExceptionHandlerExceptionResolver is created which creates
-//	 * many dependent object, including an expensive ObjectMapper instance.
-//	 *
-//	 * To enable custom @ControllerAdvice classes remove this bean.
-//	 */
-//	@Bean
-//	public HandlerExceptionResolver handlerExceptionResolver() {
-//		return new HandlerExceptionResolver() {
-//
-//			@Override
-//			public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response,
-//					Object handler, Exception ex) {
-//				return null;
-//			}
-//		};
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/tulli/com/br/scrapingtool/ScrapingService.java
+++ src/main/java/tulli/com/br/scrapingtool/ScrapingService.java
@@ -17,45 +17,6 @@ public class ScrapingService {
 
 	private static final Logger LOG = Logger.getLogger(ScrapingService.class);
 
-	public String getBodyFromUrl(String path) throws Exception {
-		LOG.info(MessageFormat.format("geting page: {0}", path));
-		RestTemplate restTemplate = new RestTemplate();
-		URI uri;
-		try {
-			uri = new URI("https://github.com/" + path);
-			return restTemplate.getForEntity(uri, String.class).getBody();
-		} catch (HttpClientErrorException ex) {
-			if (ex.getStatusCode().is4xxClientError()) {
-				throw new Exception(MessageFormat.format("status code: {0}", ex.getStatusCode()));
-			}
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	private BodyHtml getBodyFromUrlRetry(String path) {
-		boolean error = false;
-		long timeout = 50;
-		int tries = 0;
-		do {
-			try {
-				return new BodyHtml(getBodyFromUrl(path));
-			} catch (Exception e) {
-				LOG.error(e.getMessage());
-				error = true;
-				try {
-					Thread.sleep(timeout * tries);
-				} catch (InterruptedException e1) {
-					LOG.error("Error on getting page", e1);
-					Thread.currentThread().interrupt();
-				}
-				tries++;
-			}
-		} while (error);
-		return new BodyHtml();
-	}
-
 	@Cacheable("files")
 	public List<FileDetail> extractFilesFromRepository(String path) {
 		LOG.info(MessageFormat.format("geting files from: {0}", path));
@@ -63,23 +24,44 @@ public class ScrapingService {
 
 		ArrayList<FileDetail> filesDetail = new ArrayList<>();
 
-		BodyHtml body = getBodyFromUrlRetry(path + "/file-list/" + defaultBranch);
+		BodyHtml body = getBodyFromUrlWithRetry(path + "/file-list/" + defaultBranch);
 		filesDetail.addAll(body.getFiles());
 
 		processFiles(filesDetail);
 
-		for (FileDetail temp : filesDetail) {
-			if (!temp.isProcessed()) {
-				LOG.info(temp.toString());
-			}
-		}
 		return filesDetail;
 	}
 
 	private String getDefaultBranch(String path) {
-		String mainPage = getBodyFromUrlRetry(path).getBody();
+		String mainPage = getBodyFromUrlWithRetry(path).getBody();
 		int startPos = mainPage.indexOf("commits/");
 		return mainPage.substring(startPos + 8, mainPage.indexOf(".atom", startPos));
+	}
+
+	private BodyHtml getBodyFromUrlWithRetry(String path) {
+		LOG.info(MessageFormat.format("geting page: {0}", path));
+		RestTemplate restTemplate = new RestTemplate();
+		URI uri;
+
+		boolean error = false;
+		long timeout = 50;
+		int tries = 0;
+		do {
+			try {
+				uri = new URI("https://github.com/" + path);
+				return new BodyHtml(restTemplate.getForEntity(uri, String.class).getBody());
+			} catch (HttpClientErrorException e) {
+				if (e.getStatusCode().is4xxClientError()) {
+					LOG.error(MessageFormat.format("Error on getting page: status code: {0}", e.getStatusCode()));
+				}
+				error = true;
+				sleep(timeout, tries);
+				tries++;
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			}
+		} while (error);
+		return new BodyHtml();
 	}
 
 	private void processFiles(ArrayList<FileDetail> filesDetail) {
@@ -88,15 +70,23 @@ public class ScrapingService {
 				continue;
 			}
 			if (filesDetail.get(i).isFolder()) {
-				BodyHtml page = getBodyFromUrlRetry(filesDetail.get(i).getDirectoryName());
+				BodyHtml page = getBodyFromUrlWithRetry(filesDetail.get(i).getDirectoryName());
 				filesDetail.addAll(page.getFiles());
 				filesDetail.get(i).setProcessed(true);
 			} else {
-				BodyHtml data = getBodyFromUrlRetry(filesDetail.get(i).getFileName());
+				BodyHtml data = getBodyFromUrlWithRetry(filesDetail.get(i).getFileName());
 				filesDetail.get(i).setLines(data.getLines());
 				filesDetail.get(i).setSize(data.getSize());
 				filesDetail.get(i).setProcessed(true);
 			}
+		}
+	}
+
+	private void sleep(long timeout, int tries) {
+		try {
+			Thread.sleep(timeout * tries);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
 		}
 	}
 
